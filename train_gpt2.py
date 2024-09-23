7c8f29d57e4d6abced5788207545650641c17b3c
Mak-Ta-Reque <maktareq@gmail.com>
2024-09-23 17:27:06 +0200

    nothng much a comment

--- train_gpt2.py
+++ train_gpt2.py
@@ -142,6 +142,7 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
-
+#------------------------------------------------------------------------------
 model = GPT.from_pretrained('gpt2')
 print("pass")
+# Continue from 30 min
